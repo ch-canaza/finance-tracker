89ed9041daa125969095faf4c15bb085feb208e4
ch-canaza <ch-canaza@hotmail.com>
2021-03-23 14:36:50 -0400

    creating Stock model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_213918) do
+ActiveRecord::Schema.define(version: 2021_03_23_182953) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.decimal "last_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
