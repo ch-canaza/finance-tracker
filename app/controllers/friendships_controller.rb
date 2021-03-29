6d41b9e02f8a980c546d9d363d87aab04c16049c
ch-canaza <ch-canaza@hotmail.com>
2021-03-29 18:11:24 -0400

    implement (add friend) functionality

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,8 +1,17 @@
 class FriendshipsController < ApplicationController
   def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = 'Following user'
+    else
+      flash[:alert] = ' There was something wrong with the tracking request'
+    end
+    redirect_to my_friends_path
   end
 
   def destroy
+    # references the relation so we need to especify the object (.first)
     friendship = current_user.friendships.where(friend_id: params[:id]).first
     friendship.destroy
     flash[:notice] = 'Stopped following'
